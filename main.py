8f1496c47796018813ab96a1dd20cd058aa138d1
Factorial343 <107515235+Factorial343@users.noreply.github.com>
2023-03-11 14:28:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,4 +23,5 @@ from background import Start
 Dec()
 '''
 pygame.init()
+print("Hello, this is a full Pokedéx of the Unova Reigon.\nThe Unova Pokédex was introduced in Generation V through Pokémon Black & White.\nIt is the only Pokédex to contain Pokémon only found in that Generation (with the exception of Generation I).\nYou can explore any of the 156 Pokémon by typing their number into the prompt\n")
 Start()
