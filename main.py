bcd31d0dd121c224749c47b3d8f7b6c23531d7ca
Factorial343 <107515235+Factorial343@users.noreply.github.com>
2023-03-11 12:50:58 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+from background import Start
+'''def Dec():
+  choice = int(input("Enter a number 0-146 "))
+  global result
+  result = {}
+  #for i in range(0,158):
+  if choice == 0:
+    result = {n: "Victini",numb: 0, t: fir+"/"+psy, pre: none,post:none}
+  elif choice == 1:
+    result = {n: "Snivy",numb: 1, t: gra, pre: none,post:"Servine"}
+  elif choice == 2:
+    result = {n:"Servine",numb:2, t:gra, pre: "Snivy", post: "Serperior"}
+  elif choice == 3:
+    result = {n:"Servine",numb:choice, t:gra, pre: "Servine", post: none}
+  else:
+    pass
+    print()
+  
+  for key,value in result.items():
+    print(key,value)
+  Dec()
+Dec()
+'''
+pygame.init()
+Start()
